7aa829de8803d4f5e496b3f2db78e9e060022bcd
Sachin-8125 <devinewolf7@gmail.com>
2026-06-24 00:47:09 +0530

    `Update backend code to fix issues and add features`

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,53 @@
-import express from 'express';
-import 'dotenv/config';
-import {clerkMiddleware} from '@clerk/express';
-import { connectDB } from './lib/db.js';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import fs from 'fs';
-import path from 'path';
+import "dotenv/config";
 
-const app = express();
+import fs from "fs";
+import path from "path";
 
-const PORT = process.env.PORT
-const FRONTEND_URL = process.env.FRONTEND_URL
+import { clerkMiddleware } from "@clerk/express";
 
-const publicDir = path.join(process.cwd(), 'public');
+import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
+
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
-app.use(cors({origin:FRONTEND_URL, credentials: true}));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ok: true});
+  res.status(200).json({ ok: true });
 });
 
-if(fs.existsSync(publicDir)){
-    app.use(express.static(publicDir));
-    app.get("/{*any}", (req, res, next) => {
-        res.sendFile(path.join(publicDir, 'index.html'), (err)=>next(err));
-    });
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
 }
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server started on port ${PORT}`)
-    if(process.env.NODE_ENV==='production'){
-        job.start();
-    }
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
+
+  if (process.env.NODE_ENV === "production") job.start();
 });
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    try {
-        const mongoUri = process.env.MONGODB_URI
+  try {
+    const mongoUri = process.env.MONGO_URI;
 
-        if(!mongoUri){
-            throw new Error("MONGODB_URI is required")
-        }
-
-        const conn = await mongoose.connect(mongoUri);
-
-        console.log("Connected to MongoDB", conn.connection.host);
-    } catch (error) {
-        console.error("Error connecting to MongoDB", error.message);
-        process.exit(1);
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is required");
     }
+
+    const conn = await mongoose.connect(mongoUri);
+
+    console.log("MongoDB connected", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+    // 1 means failed, 0 means success
+  }
 }
